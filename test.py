2432913de5f3362d5bf0e5c536ac3ea859079ab3
Zekissss <127739753+Zekissss@users.noreply.github.com>
2023-07-05 13:37:04 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+
+
+for i in range(3, 6, 2):
+   print(i * "*")
+for i in range(8, 11):
+  print(i * "*")
+for i in range(11, 8, -1):
+  print(i * "*")
+for i in range(5, 2, -2):
+   print(i * "*")
+
